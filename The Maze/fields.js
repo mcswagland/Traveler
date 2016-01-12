6bd33749a1fe436280349c95aac76937f0ee8fd8
mcswagland <bmcfarland@harding.edu>
2016-01-11 21:08:00 -0600

    Initial Commit
    Added project

---
+++ The Maze/fields.js
@@ -0,0 +1,67 @@
+var field0 = {
+	name: "field0",
+	left: 5,
+	right: 6,
+	up: 1,
+	center: {x: 1424, y: 2374}
+}
+
+var field1 = {
+	name: "field1",
+	left: 2,
+	right: 0,
+	up: 5,
+	down: 3,
+	center: {x: 464, y: 1734}
+}
+
+var field2 = {
+	name: "field2",
+	left: 1,
+	right: 0,
+	up: 4,
+	down: 3,
+	center: {x: 1424, y: 1734}
+}
+
+var field3 = {
+	name: "field3",
+	left: 6,
+	right: 0,
+	up: 3,
+	down: 5,
+	center: {x: 2384, y: 1734}
+}
+
+var field4 = {
+	name: "field4",
+	left: 1,
+	right: 6,
+	up: 5,
+	down: 7,
+	center: {x: 464, y: 1094}
+}
+
+var field5 = {
+	name: "field5",
+	left: 3,
+	right: 6,
+	up: 6,
+	down: 3,
+	center: {x: 1424, y: 1094}
+}
+
+var field6 = {
+	name: "field6",
+	left: 5,
+	right: 1,
+	up: 5,
+	down: 3,
+	center: {x: 2384, y: 1094}
+}
+
+var field7 = {
+	name: "field7",
+	down: 4,
+	center: {x: 1424, y: 390}
+}
